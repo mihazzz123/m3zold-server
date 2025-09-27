6b4b88004769281b31b97356776fd1fb8ac6f7b0
mike <m.dokuchaev73@gmail.com>
2025-09-27 16:16:55 +0300

    Исправить сообщение в ответе проверки аутентификации для улучшения ясности

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -3,5 +3,5 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func CheckAuth(c *gin.Context) {
-    c.JSON(200, gin.H{"status": "ok", "message": "Auth check passed"})
+	c.JSON(200, gin.H{"status": "ok", "message": "Auth check passed!"})
 }

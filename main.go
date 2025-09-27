2a4e5f03308d8fd820297561a12116ae4146d42e
mike <m.dokuchaev73@gmail.com>
2025-09-27 12:03:10 +0300

    Добавить обработчики аутентификации и логирования, а также настроить CI/CD для развертывания API

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/mihazzz123/m3zold-server/handlers"
-	"github.com/mihazzz123/m3zold-server/middleware"
+	"github.com/mihazzz123/m3zold-server/api/handlers"
+	"github.com/mihazzz123/m3zold-server/api/middleware"
 
 	"github.com/gin-gonic/gin"
 )

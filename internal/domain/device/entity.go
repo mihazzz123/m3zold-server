cb2d61c16c7ece47e47352543abf248e65970581
Mikhail Dokuchaev <dokuchaev@bpm-cons.ru>
2025-11-17 13:56:34 +0300

    й

--- internal/domain/device/entity.go
+++ internal/domain/device/entity.go
@@ -1,8 +1,8 @@
 package device
 
 type Device struct {
-	ID     int    // Уникальный идентификатор
-	UserID int    // Владелец устройства
+	ID     string // Уникальный идентификатор
+	UserID string // Владелец устройства
 	Name   string // Название устройства (например, "Кухонный ESP")
 	Type   string // Тип (ESP8266, NeoPixel, Arduino и т.д.)
 	Status string // online / offline / error
